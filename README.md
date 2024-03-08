aa6e01eb8b63464762537d33251457c4a24a397f
Pablo Messa <130992694+Pmessa@users.noreply.github.com>
2024-03-08 14:07:42 -0300

    Sprint1 (#4)
    * users
    
    * products (#2)
    
    * Update README.md
    
    En este commit se agrega al readme.md información sobre la funcionalidad de la entrega.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # CoderServer
 Primera entrega backend CoderHouse
+En esta entrega se realizó un administrador de usuarios "UserManager.js" y un administrador de productos "UserProducts.js" para implementar en su App.
---
+++ UserManager.js
@@ -0,0 +1,36 @@
+class UserManager {
+    static #users = [];
+  
+    create(data) {
+      const user = {
+        id:
+          UserManager.#users.length === 0
+            ? 1
+            : UserManager.#users[UserManager.#users.length - 1].id + 1,
+        photo: data.photo,
+        email: data.email,
+        password: data.password,
+        role: 0,
+      };
+      UserManager.#users.push(user);
+      console.log("usuario creado");
+    }
+    read() {
+      return UserManager.#users;
+    }
+  }
+  const usersManager = new UserManager();
+  
+  usersManager.create({
+    photo: "foto.jpg",
+    email: "pabloserver@gmail.com",
+    password: 1234,
+    role: 1,
+  });
+  usersManager.create({
+    photo: "foto01.jpg",
+    email: "jorgeserver@gmail.com",
+    password: 5678,
+    role: 1,
+  });
+  console.log(usersManager.read());
