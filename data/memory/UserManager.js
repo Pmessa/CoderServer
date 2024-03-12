c2e349fdb2cbbd23a696822dea5b4414d29f5c0d
Pablo Messa <130992694+Pmessa@users.noreply.github.com>
2024-03-12 14:43:00 -0300

    ProductManager sincrónico (#9)

--- data/memory/UserManager.js
+++ data/memory/UserManager.js
@@ -1,15 +1,15 @@
 class UserManager {
   static #users = [];
 
-  create(data) {
+  create(user) {
     const user = {
       id:
         UserManager.#users.length === 0
           ? 1
           : UserManager.#users[UserManager.#users.length - 1].id + 1,
-      photo: data.photo,
-      email: data.email,
-      password: data.password,
+      photo: user.photo,
+      email: user.email,
+      password: user.password,
       role: 0,
     };
     UserManager.#users.push(user);
