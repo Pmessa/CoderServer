6d026ca8f92f50b6c4838cd868aabea134ab1aa2
pmessa <pablomessa@gmail.com>
2024-03-16 16:14:57 -0300

    memory

--- data/memory/ProductsManager.js
+++ data/memory/ProductsManager.js
@@ -1,5 +1,8 @@
-// Clase ProductManager para gestionar productos con métodos de creación, lectura y eliminación
-class ProductManager {
+
+const crypto = require('crypto');
+
+// Clase ProducstManager para gestionar productos con métodos de creación, lectura y eliminación
+class ProducstManager {
   static #products = []; // Array privado para almacenar los productos
 
   // Método para crear un nuevo producto
@@ -22,10 +25,13 @@ class ProductManager {
       }
       // Crear un nuevo producto con un ID único e incremental
       const newProduct = {
-        id:
-          ProductManager.#products.length === 0
-            ? 1
-            : ProductManager.#products[ProductManager.#products.length - 1].id + 1,
+      //id:crypto.randomBytes(12).toString('hex'),
+         id:product.id || crypto.randomBytes(12).toString('hex'),
+  
+          // ProducstManager.#products.length === 0
+          //   ? 1
+          //   : ProducstManager.#products[ProducstManager.#products.length - 1]
+          //       .id + 1,
         title: product.title,
         photo: product.photo,
         category: product.category,
@@ -33,8 +39,7 @@ class ProductManager {
         stock: product.stock,
       };
       // Agregar el nuevo producto al array de productos
-      ProductManager.#products.push(newProduct);
-      console.log("PRODUCTO CREADO");
+      ProducstManager.#products.push(newProduct);
     } catch (error) {
       console.log(error);
     }
@@ -43,10 +48,10 @@ class ProductManager {
   // Método para mostrar todos los productos
   read() {
     try {
-      if (ProductManager.#products.length === 0) {
+      if (ProducstManager.#products.length === 0) {
         throw new Error("No hay productos para mostrar");
       }
-      return ProductManager.#products;
+      return ProducstManager.#products;
     } catch (error) {
       console.log(error);
     }
@@ -55,7 +60,7 @@ class ProductManager {
   // Método para mostrar un producto específico por su ID
   readOne(id) {
     try {
-      const product = ProductManager.#products.find((p) => p.id === id);
+      const product = ProducstManager.#products.find((p) => p.id === id);
       if (!product) {
         throw new Error(`Producto con ID ${id} no encontrado`);
       }
@@ -70,8 +75,10 @@ class ProductManager {
     try {
       this.readOne(id); // Verificar si el producto existe
       // Filtrar el array de productos para excluir el producto con el ID dado
-      const without = ProductManager.#products.filter((each) => each.id !== id);
-      ProductManager.#products = without; // Actualizar el array de productos
+      const without = ProducstManager.#products.filter(
+        (each) => each.id !== id
+      );
+      ProducstManager.#products = without; // Actualizar el array de productos
       console.log("PRODUCTO ELIMINADO");
     } catch (error) {
       console.log(error);
@@ -79,14 +86,37 @@ class ProductManager {
   }
 }
 
-// Crear una instancia de ProductManager
-const productManager = new ProductManager();
+// Crear una instancia de ProducstManager
+const productManager = new ProducstManager();
 
 // Crear los productos
 productManager.create({
+  id:1,
   title: `MANTEQUILLA DE MANÍ`,
   photo: "mantequilla_maní.jpg",
-  category: `untables`,
+  category: `untable`,
+  price: 15000,
+  stock: 10,
+});
+productManager.create({
+  id:2,
+  title: `NUEZ PECAN`,
+  photo: "nueces.jpg",
+  category: `furtos secos`,
+  price: 13000,
+  stock: 10,
+});
+productManager.create({
+  title: `CHIA`,
+  photo: "semillaDeChia.jpg",
+  category: `semillas`,
+  price: 10000,
+  stock: 10,
+});
+productManager.create({
+  title: `GIRASOL`,
+  photo: "girasoles.jpg",
+  category: `semillas`,
   price: 15000,
   stock: 10,
 });
@@ -101,7 +131,7 @@ productManager.create({
 
 productManager.create({
   title: `TOFU`,
-  photo: "tofu.jpg",
+  photo: `"https://img.freepik.com/foto-gratis/vista-superior-arreglo-vegetal-forma-corazon_23-2148287518.jpg?w=826&t=st=1710260892~exp=1710261492~hmac=630fd8a56369a2ed0c45d9fdaaa2b19f80b9e4c4dddc2eb0ccc43f4ea6d52c27" `,
   category: `tofu`,
   price: 150,
   stock: 1000,
@@ -122,13 +152,28 @@ productManager.create({
   price: 150,
   stock: 11000,
 });
+productManager.create({
+  title: `ACEITE DE LINO`,
+  photo: "aceite de lino.jpg",
+  category: `aceites`,
+  price: 15000,
+  stock: 10,
+});
+productManager.create({
+  title: `QUINOA`,
+  photo: "quinoa.jpg",
+  category: `untables`,
+  price: 15000,
+  stock: 10,
+});
 
 // Mostrar todos los productos
 console.log(productManager.read());
 
 // Mostrar un producto específico por su ID
-console.log(productManager.readOne(5));
-
+console.log(productManager.readOne(2));
+//console.log(productManager.readOne(8));
 // Eliminar productos por su ID
-console.log(productManager.destroy(3));
+console.log(productManager.destroy(2));
+console.log(productManager.readOne(2));
 console.log(productManager.destroy(13));
