c3af19ed4a93305b8b5b4c4c6f93c9e712d7a4c5
pmessa <pablomessa@gmail.com>
2024-03-26 06:57:23 -0300

    products

--- server.js
+++ server.js
@@ -1,89 +1,61 @@
 import express from "express";
-import usersManager from "./data/fs/UsersManager.fs.js";
-//Server
+import productsManager from "./data/fs/ProductsManager.fs.js";
+
 const server = express();
-//Se crea el servidor
 const port = 8080;
-const ready = () => console.log("server ready on port" + port);
+const ready = () => console.log("server ready on port " + port);
 server.listen(port, ready);
-//Se inicia/levanta el servidor
 
+//middleware
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
 
+//routes
 
-//middelwares
-server.use(express.urlencoded({ extended: true }))
-//Obligo a mi servidor a usar la funcion encargada de leer parametros/consultas
-//permite leer req.params y req.query
+server.get("/", async (req, res) => {
+  try {
+    return res.json({
+      statusCode: 200,
+      message: "CODER API OK",
+    });
+  } catch (error) {
+    return res.json({
+      statusCode: 500,
+      message: "CODER API ERROR",
+    });
+  }
+});
+const create = async (req, res) => {
+  try {
+    const data = req.body;
+    const one = await productsManager.create(data);
+    return res.json({
+      statusCode: 201,
+      message: "CREATED ID. " + one.id,
+    });
+  } catch (error) {
+    return res.json({
+      statusCode: error.statusCode || 500,
+      message: error.message || "CODER API ERROR",
+    });
+  }
+};
 
-//Router
-server.get("/", async (requerimientos, respuesta) => {
-    try {
-        return respuesta.status(200).json({
-            responce: "CODER API",
-            success: true
-        })
-    } catch (error) {
-        console.log(error);
-        return respuesta.status(500).json({
-            response: "CODER API ERROR",
-            success: false
-        })
-    }
-})
-
-server.get("/api/users", async (req, res) => {
-    try {
-        const { role } = req.query
-        const all = await usersManager.read(role)
-        if (all.length !== 0) {
-            return res.status(200).json({
-                statusCode: 200,
-                response: all,
-                success: true
-            })
-        } else {
-            return res.status(404).json({
-                statusCode: 404,
-                response: null,
-                message: "No users found for the specified role.",
-                success: false
-            });
-        }
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            statusCode: 500,
-            response: "Internal Server Error",
-            success: false
-        })
-    }
-})
-server.get("/api/users/:uid" , async (req, res) => {
-    try {
-        const { uid } = req.params
-        const one = await usersManager.readOne(uid)
-        if (one) {
-            return res.status(200).json({
-                statusCode: 200,
-                response: one,
-                success: true
-            })
-
-        } else {
-            return res.status(404).json({
-                statusCode: 404,
-                response: null,
-                message: "User not found.",
-                success: false
-            });
-        }
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            statusCode: 500,
-            response: "Internal Server Error",
-            success: false
-        })
-        
-    }
-})
+const update = async (req, res) => {
+  try {
+    const { pid } = req.params;
+    const data = req.body;
+    const one = await productsManager.update(pid, data);
+    return res.json({
+      statusCode: 200,
+      message: "UPDATED ID: " + one.id,
+    });
+  } catch (error) {
+    return res.json({
+      statusCode: error.statusCode || 500,
+      message: error.message || "CODER API ERROR",
+    });
+  }
+};
+server.post("/api/products", create);
+server.put("/api/products/:pid", update);
