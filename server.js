644182e30ad023d8b490bf5172bf8a3baf2a54ce
Carlos Sebastián Avila <savila.dimprenta@hcdn.gob.ar>
2024-04-05 12:45:45 -0300

    users

--- server.js
+++ server.js
@@ -1,19 +1,30 @@
 import express from "express";
-import indexRouter from "./src/router/index.router.js";
-import errorHandler from "./src/middlewares/errorHandler.mid.js";
-import pathHandler from "./src/middlewares/pathHandler.mid.js";
+import { createServer } from "http";
+import { Server } from "socket.io";
 import morgan from "morgan";
 import { engine } from "express-handlebars";
 import __dirname from "./utils.js";
+
+import indexRouter from "./src/router/index.router.js";
+import socketCb from "./src/router/index.socket.js";
+import errorHandler from "./src/middlewares/errorHandler.mid.js";
+import pathHandler from "./src/middlewares/pathHandler.mid.js";
+
 
 //Server
 const server = express();
 //Se crea el servidor
 const port = 8080;
 const ready = () => console.log("server ready on port" + port);
-server.listen(port, ready);
+const nodeServer = createServer(server);
 //Se inicia/levanta el servidor
 
+//tcp server
+const socketServer = new Server(nodeServer);
+socketServer.on("connection", socketCb);
+nodeServer.listen(port, ready);
+
+//template engine
 server.engine("handlebars", engine());
 server.set('view engine', 'handlebars')
 server.set('views', __dirname+'/src/views')
