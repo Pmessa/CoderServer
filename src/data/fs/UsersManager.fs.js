d42c232a409eb2f558ea1e5693e5430e3a63ec18
Sebastian Avila <sebasavila81@gmail.com>
2024-04-07 15:36:53 -0300

    users

--- src/data/fs/UsersManager.fs.js
+++ src/data/fs/UsersManager.fs.js
@@ -66,13 +66,11 @@ class UsersManager {
         const user = {
           id: crypto.randomBytes(12).toString("hex"),
           //Si no proporcionan la foto se usa una predeterminada
-          photo:
-            data.photo ||
-            "https://cdn-icons-png.freepik.com/512/266/266033.png",
+          photo: data.photo || "https://cdn-icons-png.freepik.com/512/266/266033.png",
           email: data.email,
           password: data.password,
-          role: data.role || 0,
-        };
+          role: data.role || 0, 
+        }
         //Lee todos los usuarios del archivo
         let all = await fs.promises.readFile(this.path, "utf-8");
         //Convierte los usuarios en formato JSON a un objeto JS
--- src/routers/index.socket.js
+++ src/routers/index.socket.js
@@ -3,7 +3,7 @@ import usersManager from "../data/fs/UsersManager.fs.js";
 export default async (socket) => {
   console.log("client id: " + socket.id);
   socket.emit("users", await usersManager.read());
-  socket.on("register", async (data) => {
+  socket.on("register", async data => {
     await usersManager.create(data);
     socket.emit("users", await usersManager.read());
   });
