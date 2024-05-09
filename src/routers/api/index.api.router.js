55743f5b8cb0ff927242e497d066619c0300249e
Sebastian Avila <sebasavila81@gmail.com>
2024-05-09 20:24:24 -0300

    session

--- src/routers/api/index.api.router.js
+++ src/routers/api/index.api.router.js
@@ -3,6 +3,8 @@ import usersRouter from "./users.api.js";
 import productsRouter from "./products.api.js";
 import cookiesRouter from "./cookies.api.js";
 import cartsRouter from "./carts.api.js"
+import sessionsRouter from "./sessions.api.js";
+
 const apiRouter = Router();
 
 apiRouter.use("/users", usersRouter)
