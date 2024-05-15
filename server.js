93dd0cca5279e98f01d855201a4d66dcc1f52d72
Sebastian Avila <sebasavila81@gmail.com>
2024-05-15 20:23:20 -0300

    merge

--- server.js
+++ server.js
@@ -5,12 +5,8 @@ import { Server } from "socket.io";
 import morgan from "morgan";
 import { engine } from "express-handlebars";
 import __dirname from "./utils.js";
-<<<<<<< HEAD
-
-=======
 import expressSession from "express-session";
 import MongoStore from "connect-mongo";
->>>>>>> 1b27f727f3a68a1eac345913dec84498a2d27535
 import cookieParser from "cookie-parser";
 import indexRouter from "./src/routers/index.router.js";
 import socketCb from "./src/routers/index.socket.js";
@@ -35,9 +31,6 @@ nodeServer.listen(port, ready);
 //Se inicia/levanta el servidor
 
 //middlewares
-<<<<<<< HEAD
-server.use(cookieParser())
-=======
 server.use(
   expressSession({
     store: new MongoStore({ mongoUrl: process.env.MONGO_URI, ttl: 60 * 60 }),
@@ -48,7 +41,6 @@ server.use(
   })
 );
 server.use(cookieParser(process.env.SECRET_COOKIE))
->>>>>>> 1b27f727f3a68a1eac345913dec84498a2d27535
 server.use(express.json()); //permite leer req.params y req.query
 server.use(express.urlencoded({ extended: true }));
 server.use(express.static(__dirname + "/public"));
--- src/routers/api/cookies.api.js
+++ src/routers/api/cookies.api.js
@@ -4,13 +4,6 @@ const cookiesRouter = Router();
 
 cookiesRouter.get("/set", (req, res, next) => {
   try {
-<<<<<<< HEAD
-    return res
-      .cookie("modo", "nocturno", { maxAge: 100000 })
-      .cookie("otra", "cookie nueva", { maxAge: 60000 })
-      .cookie("online", "true", { maxAge: 60 * 60 * 1000 })
-      .json({ message: "la cookie vence en 10s" });
-=======
     return (
       res
         //con el método cookie() seteo una cookie
@@ -19,7 +12,6 @@ cookiesRouter.get("/set", (req, res, next) => {
         .cookie("online", "true", { maxAge: 60 * 60 * 1000 })
         .json({ message: "la cookie vence en 10s" })
     );
->>>>>>> 1b27f727f3a68a1eac345913dec84498a2d27535
   } catch (error) {
     return next(error);
   }
@@ -29,9 +21,6 @@ cookiesRouter.get("/", (req, res, next) => {
   try {
     const cookies = req.cookies;
     const online = req.cookies.online;
-<<<<<<< HEAD
-    return res.json({cookies, online });
-=======
     return res.json({ cookies, online });
   } catch (error) {
     return next(error);
@@ -62,13 +51,8 @@ cookiesRouter.get("/signed", (req, res, next) => {
 cookiesRouter.get("/get-signed", (req, res, next) => {
   try {
     return res.json({ message: req.signedCookies });
->>>>>>> 1b27f727f3a68a1eac345913dec84498a2d27535
   } catch (error) {
     return next(error);
   }
 });
-<<<<<<< HEAD
-
-=======
->>>>>>> 1b27f727f3a68a1eac345913dec84498a2d27535
 export default cookiesRouter;
