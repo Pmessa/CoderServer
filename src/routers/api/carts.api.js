ae6b0f42876d49bff40b0b70a8597f9d687b14a4
Sebastian Avila <sebasavila81@gmail.com>
2024-06-03 20:30:03 -0300

    users

--- src/routers/api/carts.api.js
+++ src/routers/api/carts.api.js
@@ -53,9 +53,9 @@ async function readOne(req, res, next) {
 }
 async function create(req, res, next) {
   try {
-    console.log("test")
+    //console.log("test")
     const data = req.body;
-    console.log(data);
+    //console.log(data);
     const newProduct = {
       product_id: data.product_id,
       user_id: data.user_id,
@@ -100,9 +100,9 @@ async function destroy(req, res, next) {
 }
 async function destroyAll(req, res, next) {
   try {
-    console.log("Destroy all:")
+    //console.log("Destroy all:")
     const { user_id } = req.body;
-    console.log(user_id)
+    //console.log(user_id)
     const all = await cartsManager.destroyAll({ user_id: user_id });
     return res.json({
       statusCode: 200,
